c03a943a791286ab8507abad9f4d299b3a8ea037
Jawkx <kaixuan@coingecko.com>
2021-06-25 16:47:07 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useMemo, useState } from 'react';
+
+
+const HugeList = React.memo(({arrayOfItems,itemStyle}) => {
+  console.log("rendered")
+  const itemsJxs = arrayOfItems.map((value,idx)=> <li key={idx} style = {itemStyle}>This is item {value}</li>)
+  return <ul>{itemsJxs}</ul>
+})
+
+HugeList.whyDidYouRender = true
 
 function App() {
+  //const [arrayOfItems,setArrayOfItems] = useState([...Array(5000).keys()])
+  const [items,setItems] = useState([...Array(10).keys()])
+  const [inputBox, setInputBox] = useState("")
+
+  //const itemStyle = useMemo(() => ({textAlign: "left"}) , [])
+  const itemStyle = {textAlign: "left"}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setItems([...items , inputBox])
+    setInputBox("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit = {(e) => handleSubmit(e)}> 
+        <input type = "text" value={inputBox} onChange = {(e) => setInputBox(e.target.value)}/>
+        <button> Add Item </button>
+      </form>
+      <HugeList arrayOfItems = {items} itemStyle = {itemStyle}/>
     </div>
   );
 }
